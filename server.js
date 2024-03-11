2b9da5fc88b88a113bb6875a0863b95f9443a958
anthonyero <anthonyero@gmail.com>
2024-03-10 22:11:07 -0500

    Defined a route to retrieve desired information from the role and department tables. Returning values as a formatted table still requires implementation

--- server.js
+++ server.js
@@ -44,6 +44,24 @@ app.get('/api/departments', (req, res) => {
   });
 });
 
+// View Roles
+
+app.get('/api/roles', (req, res) => {
+  const sql = `SELECT role.title AS "Job Title", role.id AS "Role ID", role.salary AS "Salary", department.name AS "Department" FROM role JOIN department ON department.id = role.department_id;`;
+
+  pool.query(sql, (err, { rows }) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    console.log(rows)
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
